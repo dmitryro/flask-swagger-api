b6cf4cb6728666f0d75d98700eec6b220da2bd8b
dmitryro <dmitryro@gmail.com>
2020-04-01 23:44:40 -0400

    upgrading

---
+++ frontend/upgrade.sh
@@ -0,0 +1,11 @@
+# Run directly through npx
+npx -p @ant-design/codemod-v4 antd4-codemod src
+
+# Or global installation
+# Use npm
+npm i -g @ant-design/codemod-v4
+# Use yarn
+yarn global add @ant-design/codemod-v4
+
+# Execute
+antd4-codemod src
